c15670f78d0556957eefc2881c2d4eed26c0f128
Senthil <spk2dc@virginia.edu>
2020-07-15 00:01:23 -0400

    trying base url instead of full path

--- SpotifyBackendServer.js
+++ SpotifyBackendServer.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 
 
 let corsOptions = {
-    origin: 'https://spk2dc.github.io/SpotifyTrackFilter/',
+    origin: 'https://spk2dc.github.io',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 app.use(cors(corsOptions))
